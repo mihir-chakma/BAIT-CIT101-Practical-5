73dffddb9b28caa59e2fb2495289800f0d7bfa71
Mihir Chakma <mihirchakmain@gmail.com>
2023-03-09 18:11:19 +0530

    updated

--- 2/a.py
+++ 2/a.py
@@ -5,6 +5,15 @@ meaningful words constructed from it.
 """
 
 input_string = input("Enter your string: ")
-five_words = input_string.split(sep=" ", maxsplit=4)
-print(five_words)
 
+print(input_string[2:6])
+print(input_string[8:13])
+print(input_string[-15:-11])
+print(input_string[23:26])
+print(input_string[-6:-4])
+print(input_string[-4:-1])
+
+
+# input_string = input("Enter your string: ")
+# five_words = input_string.split()
+# print(five_words)
--- 2/c.py
+++ 2/c.py
@@ -6,9 +6,22 @@ Write a single Python statement to generate the following pattern.
 * * * * 
 """
 
+# using doc string 
+pyramid = """
+* 
+* *
+* * *
+* * * *
+"""
+
+print(pyramid)
+
+
+
+# using for loop 
 rows = int(input("Enter the number of rows: "))
 for i in range(rows):
     for j in range(i):
         print('*', end=" ")
     print('')
-    
+
