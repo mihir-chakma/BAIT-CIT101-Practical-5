97f125e8d9add34cc159a540fdbd70b6e922f3b6
Mihir Chakma <mihirchakmain@gmail.com>
2023-03-10 14:14:40 +0530

    updated

--- 1/e.py
+++ 1/e.py
@@ -3,7 +3,7 @@ Write two Python statements in the interactive mode to display the acronym 'COVI
 form the string " Coronavirus disease of 2019"
 '''
 
-c = " Coronavirus disease of 2019"
+c = " Coronavirus disease  of  2019"
 acronym = c[1:3] + c[7:9] + c[13] + '-' + c[-2:]
 print(acronym.upper())
 
